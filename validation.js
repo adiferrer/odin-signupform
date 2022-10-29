705aaefdf71d5027ee1319dd93c5fc3ac88fb0f5
Adi <adi@macbook.local>
2022-10-29 12:16:15 -0600

    Fix password validation

--- validation.js
+++ validation.js
@@ -1,9 +1,12 @@
 const passwordError = document.getElementById('password-error');
 const submitButton = document.getElementById('submit');
 const passwords = Array.from(document.getElementsByClassName('error'));
+const inputs = 
 
 submitButton.addEventListener('click', () => {
-    if(passwords[0].value == '' || passwords[0].value != passwords[1].value) {
+
+    // password validation
+    if(passwords[0].value != passwords[1].value) {
         passwordError.textContent = '';
         const asterisk = document.createElement('sup');
         const asteriskText = document.createTextNode('*');
@@ -11,5 +14,10 @@ submitButton.addEventListener('click', () => {
 
         passwordError.insertBefore(asterisk, passwordError[0]);
         passwordError.textContent += ' Passwords do not match';
+
+        passwords.forEach(passBox => passBox.style.borderColor = 'red');
+    } else {
+        passwordError.textContent = '';
+        passwords.forEach(passBox => passBox.style.borderColor = '#E5E7EB');
     }
 })
